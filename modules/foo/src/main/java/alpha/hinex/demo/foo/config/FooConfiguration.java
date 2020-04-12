ff7259ec702d6e92ae540fe353827eb41419bd6a
Alpha Hinex <AlphaHinex@gmail.com>
2020-04-12 22:40:16 +0800

    Clean code

--- modules/foo/src/main/java/alpha/hinex/demo/foo/config/FooConfiguration.java
+++ modules/foo/src/main/java/alpha/hinex/demo/foo/config/FooConfiguration.java
@@ -2,13 +2,11 @@ package alpha.hinex.demo.foo.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("alpha.hinex.demo.foo.controller")
-@Order(-10)
 public class FooConfiguration implements WebMvcConfigurer {
 }
